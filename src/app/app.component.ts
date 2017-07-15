5d1b9df3f3b531891b0449d0fd393d9e4f7e43ab
aladinmother1107 <aladinmother1107@hotmail.com>
2017-07-15 12:15:16 +0200

    Version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  nombre = "Fernando";
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  PI = Math.PI;
+  a = 0.234;
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "Wolverine",
+    clave: "Logan",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  }
 }
